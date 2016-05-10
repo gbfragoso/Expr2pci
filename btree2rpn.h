b8703c0fe735e60366c28dd755f9a21429368b2e
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-05-10 17:11:06 -0300

    Update btree2rpn.h

--- btree2rpn.h
+++ btree2rpn.h
@@ -1,6 +1,6 @@
 #ifndef BTREE2RPN_H
 #define BTREE2RPN_H
 
-char* postOrder(char** btree, int treeSize, int node, char* expr);
+char* postOrder(char* btree, int treeSize, int node, char* expr);
 
 #endif
